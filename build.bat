e2e11cf17d150c9757a2d9ee5460bc5448d3ba8d
sultgalu <komlosi.norbii@gmail.com>
2021-09-30 21:47:06 +0200

    changed build.bat

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ cmake CMakeLists.txt -B
 cmake --build .
 copy .\bin\Debug\assimp-vc142-mtd.dll ..\..\..\..\..\Nork /Y
 copy .\bin\Debug\assimp-vc142-mtd.pdb ..\..\..\..\..\Nork /Y
+copy .\lib\Debug\assimp-vc142-mtd.lib ..\..\libs\ /Y
