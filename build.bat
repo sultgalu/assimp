8e9fdff8b57053ed5676bd1a4b620355e3632f81
norbi_sgl <norbert.komlosi@i4p.com>
2021-11-27 16:06:18 +0100

    fix build script

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
-%1\cmake CMakeLists.txt -S %~dp0 -B %~dp0
-%1\cmake --build %~dp0
+@echo off
+IF "%1"=="" set cmakePath=cmake
+IF NOT "%1"=="" set cmakePath=%1
+@echo on
+echo %cmakePath%
+%cmakePath% CMakeLists.txt -S %~dp0 -B %~dp0
+%cmakePath% --build %~dp0
+@echo off
 copy %~dp0bin\Debug\assimp-vc142-mtd.dll %~dp0..\..\..\..\..\Nork /Y
 copy %~dp0bin\Debug\assimp-vc142-mtd.pdb %~dp0..\..\..\..\..\Nork /Y
 copy %~dp0lib\Debug\assimp-vc142-mtd.lib %~dp0..\..\libs\ /Y
